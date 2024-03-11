efef0681bf757637df462758a39ceb174a2b5f62
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-03-11 09:59:57 +0100

    docs: Update documentation and attribute views for handling 404 and 500 errors

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,17 +6,32 @@ URL Patterns:
     - '/lettings/': Includes URL patterns from the 'lettings' application.
     - '/profiles/': Includes URL patterns from the 'profiles' application.
     - '/admin/': Maps to the Django admin interface.
+
+Additionally, custom handlers for error pages are defined to provide a better user experience for
+HTTP 404 and 500 errors.
+
+Attributes:
+    urlpatterns (list): A list of `path` instances that maps URLs to views.
+    handler404 (str): The path to the view function for handling 404 errors.
+    handler500 (str): The path to the view function for handling 500 errors.
 """
 
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf.urls import handler404
+from django.conf.urls import handler500
 
 from . import views
 
+# URL configuration for the site, mapping URLs to views.
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # Home page
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Django admin site
 ]
+
+# Custom error handlers
+handler404 = 'oc_lettings_site.views.error_404'  # noqa: F811
+handler500 = 'oc_lettings_site.views.error_500'  # noqa: F811
