f8d77e0a3fb092c9368ecf46f61909112dba67c3
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2024-03-11 10:01:00 +0100

    feat: Add custom 404 and 500 error handling views

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -3,6 +3,8 @@ This module contains a view to render the home page.
 
 Function:
     - index(request): Renders the home page.
+    - error_404(request, exception): Renders a custom 404 error page.
+    - error_500(request): Renders a custom 500 error page.
 """
 
 from django.shortcuts import render
@@ -25,3 +27,30 @@ def index(request):
     - An HTTP response rendering the 'index.html' template.
     """
     return render(request, 'index.html')
+
+
+def error_404(request, exception):
+    """
+    Renders a custom 404 error page.
+
+    Parameters:
+    - request (HttpRequest): The HTTP request object.
+    - exception (Exception): The exception that caused the 404 error.
+
+    Returns:
+    - HttpResponse: The response object with the rendered '404.html' template and 404 status code.
+    """
+    return render(request, '404.html', {}, status=404)
+
+
+def error_500(request):
+    """
+    Renders a custom 500 error page.
+
+    Parameters:
+    - request (HttpRequest): The HTTP request object.
+
+    Returns:
+    - HttpResponse: The response object with the rendered '500.html' template and 500 status code.
+    """
+    return render(request, '500.html', {}, status=500)
